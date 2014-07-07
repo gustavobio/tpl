316235f94daf3d2783b5bc15dcda2bb5d7e87763
Gustavo Carvalho <gustavo.bio@gmail.com>
2014-07-07 20:44:56 -0300

    New dataset containing family synonymy

---
+++ R/apg.R
@@ -0,0 +1,10 @@
+#' APG families
+#' 
+#' Complete synonymy of family names 
+#' 
+#' @docType data
+#' @keywords datasets
+#' @format A data frame with 1597 rows and 3 columns
+#' @note Downloaded from \url{http://www.mobot.org/MOBOT/research/APweb/}
+#' @name apg
+NULL
